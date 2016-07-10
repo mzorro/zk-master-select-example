bc37a469808ea092ec64f3bae02c142bdaaa7882
mzorro <mzorrox@gmail.com>
2016-07-10 10:10:10 +0800

    测试1

--- src/main/java/me/mzorro/zookeeper/BindMasterIPHelper.java
+++ src/main/java/me/mzorro/zookeeper/BindMasterIPHelper.java
@@ -10,23 +10,34 @@ import java.io.InputStreamReader;
 public class BindMasterIPHelper {
 
     private static void runCommond(String cmd) {
-        BufferedReader reader = null;
+        BufferedReader inputReader = null, errorReader = null;
         try {
             Process process = Runtime.getRuntime().exec(cmd);
             if (process != null) {
-                reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
                 System.out.println("Execute cmd: " + cmd);
+                inputReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+                errorReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
                 String line;
-                while ((line = reader.readLine()) != null) {
+                while ((line = inputReader.readLine()) != null) {
+                    System.out.println(line);
+                }
+                while ((line = errorReader.readLine()) != null) {
                     System.out.println(line);
                 }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            if (reader != null) {
+            if (inputReader != null) {
                 try {
-                    reader.close();
+                    inputReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (errorReader != null) {
+                try {
+                    errorReader.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -36,16 +47,16 @@ public class BindMasterIPHelper {
     }
 
     public static void upMasterIP(String localIFace, String localIP, String masterIP) {
-        runCommond(String.format("ssh -l root %s \"/sbin/ifconfig %s:0 %s broadcast %s netmask 255.255.255.255 up\"",
+        runCommond(String.format("ssh -l root %s /sbin/ifconfig %s:0 %s broadcast %s netmask 255.255.255.255 up",
                 localIP, localIFace, masterIP, masterIP));
     }
 
     public static void downRemoteMasterIP(String remoteIFace, String remoteIP) {
-        runCommond(String.format("ssh -l root %s \"/sbin/ifconfig %s:0 down\"",
+        runCommond(String.format("ssh -l root %s /sbin/ifconfig %s:0 down",
                 remoteIP, remoteIFace));
     }
 
     public static void main(String[] args) {
-        runCommond("ls -al");
+        runCommond("ssh -l root 10.82.60.69 /sbin/ifconfig enp0s31f6:0 10.82.60.50 broadcast 10.82.60.50 netmask 255.255.255.255 up");
     }
 }
